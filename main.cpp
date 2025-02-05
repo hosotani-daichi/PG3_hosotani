135a45a3a8bb2f42449444cc60ec51a2abd151ce
細谷　大智 <146915322+hosotani-daichi@users.noreply.github.com>
2025-02-05 17:27:08 +0900

    07_01

--- main.cpp
+++ main.cpp
@@ -1,58 +1,29 @@
-
-#include <string>
-#include <chrono>
 #include <iostream>
-
-//コピー時間計算出力
-void measureCopyTime(const std::string& str) {
-
-    //開始
-    auto start = std::chrono::high_resolution_clock::now();
-
-    //内容コピー
-    std::string copy = str;
-
-    //終了
-    auto end = std::chrono::high_resolution_clock::now();
-
-    //時間計算
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-
-    //出力
-    std::cout << "コピー： " << duration << "μs" << std::endl;
-}
-
-//移動時間計算＋出力
-void measureMoveTime(std::string&& str) {
-
-    //開始
-    auto start = std::chrono::high_resolution_clock::now();
-
-    //内容移動
-    std::string moved = std::move(str);
-
-    //終了
-    auto end = std::chrono::high_resolution_clock::now();
-
-    //時間計算
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-
-    //出力
-    std::cout << "移動： " << duration << "μs" << std::endl;
-}
+#include <thread>
+#include <chrono>
+#include <stdio.h>
+#include <string>
 
 int main() {
+	// 初期化
+	std::string original(100000, 'a');
 
-    //文字列string with 1000,000 'a'
-    std::string a(1000000, 'a');
+	// 計測
+	auto origin = std::chrono::high_resolution_clock::now();
+	std::string copy = original;
+	auto Copy = std::chrono::high_resolution_clock::now();
 
-    std::cout << "1000000文字を移動とコピーで比較" << std::endl;
+	// 移動操作
+	auto  originMove = std::chrono::high_resolution_clock::now();
+	std::string moved = std::move(original);
+	auto Move = std::chrono::high_resolution_clock::now();
 
-    //コピー
-    measureCopyTime(a);
+	// 計測結果表示
+	auto copyDuration = std::chrono::duration_cast<std::chrono::microseconds>(Copy - origin).count();
+	auto moveDuration = std::chrono::duration_cast<std::chrono::microseconds>(Move - originMove).count();
 
-    //移動
-    measureMoveTime(std::move(a));
+	std::cout << "コピーの時間: " << copyDuration << " microSecond\n";
+	std::cout << "移動操作時間: " << moveDuration << " microSecond\n";
 
-    return 0;
+	return 0;
 }
